016967cf33322b13c7b6b521b813fee59525eae3
s17kf <s17kf@users.noreply.github.com>
2021-12-06 10:08:53 +0100

    day6 done

--- day6/solve6.py
+++ day6/solve6.py
@@ -7,7 +7,8 @@ isEmpty = common.isEmpty
 HELP_INFO = [
 "Script is solving tasks for day 6 of advent of code 2021",
 "Arguments:",
-common.TAB + "input file"
+common.TAB + "input file",
+common.TAB + "number of days to simulate"
 ]
 
 argumentsKeywords = ["inputFile", "daysToSymulate"]
@@ -28,88 +29,29 @@ initialTimer = 8
 
 daysLeftList = [int(days) for days in inputLines[0].split(",")]
 
-print(daysLeftList)
-
-# daysToSymulate = 80
 daysToSymulate = int(scriptArguments["daysToSymulate"])
 
-dayToFishCount = {}
-dayToBabyFishCount = {}
+dayToFishCount = [0,0,0,0,0,0,0]
+dayToBabyFishCount = [0,0,0,0,0,0,0]
 
 for daysLeft in daysLeftList:
-    common.addOrIncreaseForKey(dayToFishCount, daysLeft)
+    dayToFishCount[daysLeft] += 1
 
 print(dayToFishCount)
-fishes = len(daysLeftList)
 
-def handleBabyFishesForDayInPeriod(day, dayToFishCount, dayToBabyFishCount, fishes):#, oneDayToAdult, twoDaysToAdult, fishes):
-    # twoDaysToAdult = dayToFishCount[day]
-    # common.addOrIncreaseForKey(dayToFishCount, (day+2)%period, oneDayToAdult, oneDayToAdult)
-    # common.addOrIncreaseForKey(dayToBabyFishCount, (day+2)%period, twoDaysToAdult, twoDaysToAdult)
-    if day in dayToFishCount:
-        dayToBabyFishCount[(day + 2) % period] = dayToFishCount[day]
-        fishes += dayToFishCount[day]
-    # oneDayToAdult = twoDaysToAdult
-    # twoDaysToAdult = dayToFishCount[day]
-    # fishes += twoDaysToAdult
-    return fishes #, oneDayToAdult, twoDaysToAdult
+def generateBabyFishesForDayInPeriod(day, dayToFishCount, dayToBabyFishCount):
+    dayToBabyFishCount[(day + 2) % period] = dayToFishCount[day % period]
+    return dayToFishCount, dayToBabyFishCount
 
-def growUpBabyFishesForDayInPeriod(day, dayToFishCount, dayToBabyFishCount, fishes):#, oneDayToAdult, twoDaysToAdult, fishes):
-    if day in dayToBabyFishCount:
-        common.addOrIncreaseForKey(dayToFishCount, day, dayToBabyFishCount[day], dayToBabyFishCount[day])
-        del dayToBabyFishCount[day]
+def growUpBabyFishesForDayInPeriod(day, dayToFishCount, dayToBabyFishCount):
+    growUpDay = (day -1) % period
+    dayToFishCount[growUpDay] += dayToBabyFishCount[growUpDay]
+    dayToBabyFishCount[growUpDay] = 0
+    return dayToFishCount, dayToBabyFishCount
 
-def countAllFishes(dayToFishCount, dayToBabyFishCount):
-    count = 0
-    for key in dayToFishCount:
-        count += dayToFishCount[key]
-    for key in dayToBabyFishCount:
-        count += dayToBabyFishCount[key]
-    return count
+for day in range(daysToSymulate):
+    dayToFishCount, dayToBabyFishCount = generateBabyFishesForDayInPeriod(day, dayToFishCount, dayToBabyFishCount)
+    dayToFishCount, dayToBabyFishCount = growUpBabyFishesForDayInPeriod(day, dayToFishCount, dayToBabyFishCount)
 
-twoDaysToAdult = 0
-oneDayToAdult = 0
-
-for i in range(daysToSymulate + 1):
-    dayOfPeriod = i % period
-    # if dayOfPeriod in dayToFishCount or oneDayToAdult > 0:
-    #     # fishes, oneDayToAdult, twoDaysToAdult = handleBabyFishesForDayInPeriod(dayOfPeriod, dayToFishCount, oneDayToAdult, twoDaysToAdult, fishes)
-    #     fishes = handleBabyFishesForDayInPeriod(dayOfPeriod, dayToFishCount, dayToBabyFishCount, fishes)
-    fishes = handleBabyFishesForDayInPeriod(dayOfPeriod, dayToFishCount, dayToBabyFishCount, fishes)
-    growUpBabyFishesForDayInPeriod((dayOfPeriod-1)%period, dayToFishCount, dayToBabyFishCount, fishes)
-    # else:
-    #     oneDayToAdult = twoDaysToAdult
-    #     twoDaysToAdult = 0    
-    # print(f"{i+1}: {fishes} {twoDaysToAdult} {oneDayToAdult}")
-    tmpFishes = countAllFishes(dayToFishCount, dayToBabyFishCount)
-    print(f"{i+1}: {tmpFishes}: {len(dayToFishCount)}: {dayToFishCount} {len(dayToBabyFishCount)}:{dayToBabyFishCount}")
-
-# for day in range (1, daysToSymulate+1):
-#     for i in range(len(daysLeftList)):
-#         daysLeft = daysLeftList[i]
-#         if daysLeft == 0:
-#             daysLeftList.append(initialTimer)
-#         daysLeft = daysLeft - 1 if daysLeft >= period else (daysLeft - 1) % period
-#         daysLeftList[i] = daysLeft
-    # print(f"{len(daysLeftList)}: {daysLeftList}")
-
-# result = 0
-# for key in dayToFishCount:
-#     result += dayToFishCount[key]
-
-# result += oneDayToAdult
-# result += twoDaysToAdult
-
-result = countAllFishes(dayToFishCount, dayToBabyFishCount)
-
-# for i in range(-7, 8):
-#     print(f"{i} % 7 = {i%7}")
-
-
-result1 = len(daysLeftList)
-
-print(f"task1: {fishes}")
-print(f"task2: {result}")
-
-
-
+result = sum(dayToFishCount) + sum(dayToBabyFishCount)
+print(f"result: {result}")
