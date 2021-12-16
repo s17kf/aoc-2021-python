6584f757fe5fe668b70e026f0e4e3da973535d2b
s17kf <s17kf@users.noreply.github.com>
2021-12-16 22:41:47 +0100

    day 16 done

--- common/input_parser.py
+++ common/input_parser.py
@@ -12,7 +12,7 @@ def read_lines_from_file(filename):
 
 def read_script_arguments(argument_keywords):
     argument_to_value_map = {}
-    if len(sys.argv) != len(argument_keywords) + 1:
+    if len(sys.argv) != len(argument_keywords) + 1 or sys.argv[1] == '':
         return argument_to_value_map
     for i in range(len(argument_keywords)):
         argument_to_value_map[argument_keywords[i]] = sys.argv[i + 1]
@@ -21,7 +21,8 @@ def read_script_arguments(argument_keywords):
 
 def parse_arguments(arguments_keywords, error_info):
     script_arguments = read_script_arguments(arguments_keywords)
-    if common.is_empty(script_arguments):
+    if len(script_arguments) == 0:
         common.print_array_line_by_line(error_info)
         return None
+    print(len(script_arguments))
     return script_arguments
--- day16/solve16.py
+++ day16/solve16.py
@@ -1,11 +1,91 @@
 #!/bin/bash
 """exec" "pyenv" "exec" "python" "$0" "$@"""
+import sys
 
 import common
-from enum import Enum, auto
-from collections import Counter
 import numpy
-from common import numpy_matrix
+
+OPERATIONS = {
+    0: lambda a: sum(a),
+    1: lambda a: numpy.prod(a),
+    2: lambda a: min(a),
+    3: lambda a: max(a),
+    4: "literal",
+    5: lambda a: int(a[0] > a[1]),
+    6: lambda a: int(a[0] < a[1]),
+    7: lambda a: int(a[0] == a[1])
+}
+
+
+def remove_first_n_bits(bin_data, n):
+    popped_bits = bin_data[:n]
+    for i in range(n):
+        bin_data.pop(0)
+    return popped_bits
+
+
+def pop_packet_version_and_type_id(bin_data):
+    version = int(''.join(bin_data[:3]), 2)
+    type_id = int(''.join(bin_data[3:6]), 2)
+    for _ in range(6):
+        bin_data.pop(0)
+    return version, type_id
+
+
+def pop_literal_packet(bin_data):
+    data = ""
+    first_bit_in_group = 1
+    while first_bit_in_group != '0':
+        first_bit_in_group = bin_data.pop(0)
+        group = bin_data[:4]
+        data += ''.join(group)
+        remove_first_n_bits(bin_data, 4)
+    return data
+
+
+def calculate_value(bin_data, versions, values):
+    version, type_id = pop_packet_version_and_type_id(bin_data)
+    versions.append(version)
+    if type_id == 4:
+        result = int(pop_literal_packet(bin_data), 2)
+        values.append(result)
+    else:
+        length_type_id = bin_data.pop(0)
+        if length_type_id == '0':
+            length = int(''.join(remove_first_n_bits(bin_data, 15)), 2)
+            subpacket = remove_first_n_bits(bin_data, length)
+            result = OPERATIONS[type_id](calculate_value_type0(subpacket, versions))
+            values.append(result)
+        else:
+            packets_count = int(''.join(remove_first_n_bits(bin_data, 11)), 2)
+            result = OPERATIONS[type_id](calculate_value_type1(bin_data, packets_count, versions))
+            values.append(result)
+    return versions, result
+
+
+def calculate_value_type0(bin_data, versions):
+    values = []
+    while len(bin_data) > 0:
+        calculate_value(bin_data, versions, values)
+    return values
+
+
+def calculate_value_type1(bin_data, packets, versions):
+    values = []
+    for _ in range(packets):
+        calculate_value(bin_data, versions, values)
+    return values
+
+
+def solve_packet(data):
+    bin_data = ""
+    for hex_digit in data:
+        bin_data += format(int(hex_digit, 16), '04b')
+    bin_data = [bit for bit in bin_data]
+    return calculate_value(bin_data, [], [])
+
+
+print(sys.argv)
 
 HELP_INFO = [
     "Script is solving task 16 of advent of code 2021",
@@ -22,120 +102,7 @@ input_file_name = script_arguments["inputFile"]
 print("solving file: " + input_file_name)
 input_lines = common.read_lines_from_file(input_file_name)
 
-
-def remove_first_n_bits(bin_data, n):
-    for i in range(n):
-        if len(bin_data) == 0:
-            return
-        bin_data.pop(0)
-
-
-def pop_packet_version_and_type_id(bin_data):
-    version = int(''.join(bin_data[:3]), 2)
-    type_id = int(''.join(bin_data[3:6]), 2)
-    for _ in range(6):
-        bin_data.pop(0)
-    return version, type_id
-
-
-def get_packet_version_and_type_id(bin_data):
-    version = int(''.join(bin_data[:3]), 2)
-    type_id = int(''.join(bin_data[3:6]), 2)
-    return version, type_id
-
-
-def pop_zeros_at_beginning(bin_data):
-    while len(bin_data) > 0 and bin_data[0] == '0':
-        bin_data.pop(0)
-
-
-def pop_trailing_zeros(bin_data):
-    while len(bin_data) > 0 and bin_data[-1] == '0':
-        bin_data.pop()
-
-
-def pop_literal_packet(bin_data):
-    data = ""
-    first_bit_in_group = 1
-    while first_bit_in_group != '0':
-        first_bit_in_group = bin_data.pop(0)
-        group = bin_data[:4]
-        data += ''.join(group)
-        remove_first_n_bits(bin_data, 4)
-    # print(f"literal: {data}")
-    return data
-
-def get_literal_packet(bin_data):
-    data = ""
-    first_bit_in_group = 1
-    position = 0
-    while first_bit_in_group != '0':
-        first_bit_in_group = bin_data[0]
-        group = bin_data[1:5]
-        data += ''.join(group)
-    # print(f"literal: {data}")
-    return data
-
-
-def get_subpackets(bin_data):
-    length_type_bit = bin_data.pop(0)
-    if length_type_bit == '0':
-        length_of_subpackets_length = 15
-        subpackets_length = int(''.join(bin_data[:length_of_subpackets_length]), 2)
-        remove_first_n_bits(bin_data, length_of_subpackets_length)
-        subpackets_bits = bin_data[:subpackets_length]
-        remove_first_n_bits(bin_data, subpackets_length)
-        return subpackets_bits
-    else:
-        length_of_subpackets_count = 11
-        subpackets_count = int(''.join(bin_data[:length_of_subpackets_count]), 2)
-        remove_first_n_bits(bin_data, length_of_subpackets_count)
-        bin_data_copy = bin_data.copy()
-        subpackets_bits = []
-        for _ in range(subpackets_count):
-            version, type_id = pop_packet_version_and_type_id(bin_data_copy)
-            if type_id == 4:
-                subpackets_bits.append(bin_data[:6 + get_literal_packet(bin_data_copy) + 1])
-                remove_first_n_bits(bin_data, 6 + get_literal_packet(bin_data_copy))
-            else:
-                subpackets_bits.append(bin_data[:6 + 1 + len(get_subpackets(bin_data_copy)) + 1])
-                remove_first_n_bits(bin_data, 6 + 1 + len(get_subpackets(bin_data_copy)))
-        # remove_first_n_bits(bin_data, len(subpackets_bits))
-        return subpackets_bits
-
-
-def calculate_value(version, type_id, bin_data, versions):
-    calculated_values = []
-    if type_id == 4:
-        calculated_values.append(int(pop_literal_packet(bin_data)))
-    else:
-        subpackets_bits_list = get_subpackets(bin_data)
-        print(subpackets_bits_list)
-
-
-def get_transmition_value(data, is_hex=True):
-    binary_data = data
-    if is_hex:
-        binary_data = ""
-        for hex in data:
-            binary_data += format(int(hex, 16), '04b')
-    print(binary_data)
-    binary_data = [bit for bit in binary_data]
-    if is_hex:
-        pop_trailing_zeros(binary_data)
-    versions = []
-    # while len(binary_data) > 0:
-    version, type_id = pop_packet_version_and_type_id(binary_data)
-    versions.append(version)
-    if type_id == 4:
-        return int(pop_literal_packet(binary_data), 2)
-    else:
-        return calculate_value(version, type_id, binary_data, versions)
-
-
-
-result1 = 0
-result2 = get_transmition_value(input_lines[0])
-
-print(f"task1: {sum([int(version) for version in result1])} {result1}")
+result1, result2 = solve_packet(input_lines[0])
+print()
+print(f"task1: {sum(result1)} <-- {result1}")
 print(f"task2: {result2}")
